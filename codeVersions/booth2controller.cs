74a2cb5c78f679c7d64601f5bda68cc64150fc04
marfinch <76139974+marfinch@users.noreply.github.com>
2022-03-28 21:26:25 -0400

    Add files via upload

---
+++ codeVersions/booth2controller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class booth2controller : MonoBehaviour
+{
+    private Animator doorAnim;
+
+    private bool doorOpen = false;
+
+    private void Awake ()
+    {
+        doorAnim = gameObject.GetComponent <Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        if (!doorOpen)
+        {
+            doorAnim.Play("DoorOpen", 0, 0.0f);
+            doorOpen = true;
+        }
+        else
+        {
+            doorAnim.Play("Doorclose", 0, 0.0f);
+            doorOpen = false;
+
+        }
+    }
+}
